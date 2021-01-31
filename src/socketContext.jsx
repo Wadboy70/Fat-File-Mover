3d7d9e87123ec3743d66f73bd6dc78f9d8589da2
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2021-01-31 08:36:38 -0500

    cors problem

--- src/socketContext.jsx
+++ src/socketContext.jsx
@@ -3,7 +3,12 @@ import { io } from "socket.io-client";
 
 export const WebSocketContext = createContext(null);
 
-const socket = io("https://fat-file-transfer.ue.r.appspot.com");
+const socket = io("https://fat-file-transfer.ue.r.appspot.com", {
+    withCredentials: true,
+    extraHeaders: {
+      "Access-Control-Allow-Origin": true
+    }
+  });
 
 
 socket.on("files", files => {
